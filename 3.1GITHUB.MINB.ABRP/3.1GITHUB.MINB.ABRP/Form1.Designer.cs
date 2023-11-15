d2e2ab3f30ed1c0fb805a5eab27579b8e6766eee
MARÍA ISABEL NÚÑEZ BAÑÓN <marnunban@alu.edu.gva.es>
2023-11-15 18:39:13 +0100

    Se ha añadido el formulario y el codigo MINB2324

--- 3.1GITHUB.MINB.ABRP/3.1GITHUB.MINB.ABRP/Form1.Designer.cs
+++ 3.1GITHUB.MINB.ABRP/3.1GITHUB.MINB.ABRP/Form1.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            fdgg = new Label();
+            label2 = new Label();
+            txtPrecio = new TextBox();
+            cbUrgente = new CheckBox();
+            txtTelegrama = new RichTextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(493, 340);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // fdgg
+            // 
+            fdgg.AutoSize = true;
+            fdgg.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            fdgg.Location = new Point(247, 65);
+            fdgg.Name = "fdgg";
+            fdgg.Size = new Size(39, 15);
+            fdgg.TabIndex = 1;
+            fdgg.Text = "Texto";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(161, 348);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Coste:";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(247, 340);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(100, 23);
+            txtPrecio.TabIndex = 3;
+            // 
+            // cbUrgente
+            // 
+            cbUrgente.AutoSize = true;
+            cbUrgente.Location = new Point(270, 293);
+            cbUrgente.Name = "cbUrgente";
+            cbUrgente.Size = new Size(73, 19);
+            cbUrgente.TabIndex = 4;
+            cbUrgente.Text = "Urgente?";
+            cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // txtTelegrama
+            // 
+            txtTelegrama.Location = new Point(247, 95);
+            txtTelegrama.Name = "txtTelegrama";
+            txtTelegrama.Size = new Size(321, 96);
+            txtTelegrama.TabIndex = 5;
+            txtTelegrama.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtTelegrama);
+            Controls.Add(cbUrgente);
+            Controls.Add(txtPrecio);
+            Controls.Add(label2);
+            Controls.Add(fdgg);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label fdgg;
+        private Label label2;
+        private TextBox txtPrecio;
+        private CheckBox cbUrgente;
+        private RichTextBox txtTelegrama;
     }
 }
